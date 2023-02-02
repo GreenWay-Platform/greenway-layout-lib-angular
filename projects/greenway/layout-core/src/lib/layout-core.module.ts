1621f5faf9def8c74dbd7ba36a93c352a2e71f28
Matias Camiletti <matias.camiletti@gmail.com>
2023-02-02 19:58:49 -0300

    Create structure initial library core.

---
+++ projects/greenway/layout-core/src/lib/layout-core.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { LayoutCoreComponent } from './layout-core.component';
+
+
+
+@NgModule({
+  declarations: [
+    LayoutCoreComponent
+  ],
+  imports: [
+  ],
+  exports: [
+    LayoutCoreComponent
+  ]
+})
+export class LayoutCoreModule { }
---
+++ projects/greenway/layout-core/src/public-api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of layout-core
+ */
+
+export * from './lib/layout-core.service';
+export * from './lib/layout-core.component';
+export * from './lib/layout-core.module';
